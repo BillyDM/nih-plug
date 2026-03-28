57c8e47de03cb8d9a5b5455caf65ca2b6d55edf3
Billy Messenger <billydm@noreply.codeberg.org>
2026-03-27 19:57:32 -0500

    add egui integration

---
+++ crates/egui-baseview/src/lib.rs
@@ -0,0 +1,9 @@
+mod renderer;
+mod translate;
+mod window;
+
+pub use window::{EguiWindow, KeyCapture, Queue};
+
+pub use renderer::GraphicsConfig;
+
+pub use keyboard_types::Key;
---
+++ examples/gain_egui/src/main.rs
@@ -0,0 +1,5 @@
+use gain_egui::Gain;
+
+fn main() {
+    nih_plug::prelude::nih_export_standalone::<Gain>();
+}
